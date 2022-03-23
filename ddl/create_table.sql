bad9eef642f707aab4ebbdb55a660e4e3404b3a2
Murilo Ferreira <fferreira.murilo@gmail.com>
2022-03-23 00:23:11 -0300

    Implements table creation on docker image building

---
+++ ddl/create_table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE book_info (
+    title varchar(200),
+    rating int,
+    category varchar(30),
+    upc varchar(30),
+    product_type varchar(30),
+    price real,
+    price_withtax real,
+    tax_value real,
+    available_number int,
+    review_number int,
+    date date,
+    year varchar(10),
+    month varchar(10),
+    day varchar(10)
+);
